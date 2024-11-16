33ff9a419ccef286134f4f2c984f3965b0358e19
SDUST-704-mengchuan <1836537351@qq.com>
2024-11-16 20:53:31 +0800

    新建模板Template

---
+++ Template/User/main.c
@@ -0,0 +1,18 @@
+#include "gd32f4xx.h"
+#include "systick.h"
+#include <stdio.h>
+#include "main.h"
+
+/*!
+    \brief    main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    systick_config();
+
+    while(1) {
+    }
+}
